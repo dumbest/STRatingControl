64ad389f143b730270ce2d8f0fedc3e840ce8639
Thanakrit Weekhamchai <thanakrit.weekhamchai@clbs.co.th>
2016-09-24 19:05:45 +0700

    Aligned buttons center

--- Pod/Classes/STRatingControl.swift
+++ Pod/Classes/STRatingControl.swift
@@ -9,136 +9,142 @@
 import UIKit
 
 @objc public protocol STRatingControlDelegate {
-  @objc optional func didSelectRating(_ control: STRatingControl, rating: Int)
+    @objc optional func didSelectRating(_ control: STRatingControl, rating: Int)
 }
 
 @IBDesignable
 
 open class STRatingControl: UIView {
-  
-  // MARK: Properties
-  
-  @IBInspectable open var rating : Int = 0 {
-    didSet {
-      if rating < 0 {
-        rating = 0
-      }
-      if rating > maxRating {
-        rating = maxRating
-      }
-      setNeedsLayout()
+    
+    // MARK: Properties
+    
+    @IBInspectable open var rating : Int = 0 {
+        didSet {
+            if rating < 0 {
+                rating = 0
+            }
+            if rating > maxRating {
+                rating = maxRating
+            }
+            setNeedsLayout()
+        }
     }
-  }
-  @IBInspectable var maxRating : Int = 5 {
-    didSet {
-      setNeedsLayout()
+    @IBInspectable var maxRating : Int = 5 {
+        didSet {
+            setNeedsLayout()
+        }
     }
-  }
-  @IBInspectable var filledStarImage : UIImage? {
-    didSet {
-      setNeedsDisplay()
+    @IBInspectable var filledStarImage : UIImage? {
+        didSet {
+            setNeedsDisplay()
+        }
     }
-  }
-  @IBInspectable var emptyStarImage : UIImage? {
-    didSet {
-      setNeedsDisplay()
+    @IBInspectable var emptyStarImage : UIImage? {
+        didSet {
+            setNeedsDisplay()
+        }
     }
-  }
-  @IBInspectable var spacing : Int = 5 {
-    didSet {
-      setNeedsDisplay()
+    @IBInspectable var spacing : Int = 5 {
+        didSet {
+            setNeedsDisplay()
+        }
     }
-  }
-  
-  weak open var delegate : STRatingControlDelegate?
-  
-  fileprivate var ratingButtons = [UIButton]()
-  fileprivate var buttonSize : Int {
-    return Int(self.frame.height)
-  }
-  fileprivate var width : Int {
-    return (buttonSize + spacing) * maxRating
-  }
-  
-  // MARK: Initialization
-  
-  func initRate() {
-    if ratingButtons.count == 0 {
-      
-      for _ in 0..<maxRating {
-        let button = UIButton()
+    
+    private var padding: CGFloat = 0.0
+    
+    weak open var delegate : STRatingControlDelegate?
+    
+    fileprivate var ratingButtons = [UIButton]()
+    fileprivate var buttonSize : Int {
+        return Int(self.frame.height)
+    }
+    fileprivate var width : Int {
+        return (buttonSize + spacing) * maxRating
+    }
+    
+    // MARK: Initialization
+    
+    func initRate() {
+        if ratingButtons.count == 0 {
+            
+            for _ in 0..<maxRating {
+                let button = UIButton()
+                
+                button.setImage(emptyStarImage, for: UIControlState())
+                button.setImage(filledStarImage, for: .selected)
+                button.setImage(filledStarImage, for: [.highlighted, .selected])
+                button.isUserInteractionEnabled = false
+                
+                button.adjustsImageWhenHighlighted = false
+                ratingButtons += [button]
+                addSubview(button)
+            }
+        }
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
         
-        button.setImage(emptyStarImage, for: UIControlState())
-        button.setImage(filledStarImage, for: .selected)
-        button.setImage(filledStarImage, for: [.highlighted, .selected])
-        button.isUserInteractionEnabled = false
+        self.initRate()
         
-        button.adjustsImageWhenHighlighted = false
-        ratingButtons += [button]
-        addSubview(button)
-      }
+        padding = max(0.0, (bounds.width - CGFloat(width)) / 2.0)
+        
+        // Set the button's width and height to a square the size of the frame's height.
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus spacing.
+        for (index, button) in ratingButtons.enumerated() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing)) + padding
+            button.frame = buttonFrame
+        }
+        updateButtonSelectionStates()
     }
-  }
-  
-  override open func layoutSubviews() {
-    super.layoutSubviews()
     
-    self.initRate()
+    override open var intrinsicContentSize : CGSize {
+        return CGSize(width: width, height: buttonSize)
+    }
     
-    // Set the button's width and height to a square the size of the frame's height.
-    var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+    }
     
-    // Offset each button's origin by the length of the button plus spacing.
-    for (index, button) in ratingButtons.enumerated() {
-      buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
-      button.frame = buttonFrame
+    // MARK: Gesture recognizer
+    
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        handleStarTouches(touches, withEvent: event)
     }
-    updateButtonSelectionStates()
-  }
-  
-  override open var intrinsicContentSize : CGSize {
-    return CGSize(width: width, height: buttonSize)
-  }
-  
-  func updateButtonSelectionStates() {
-    for (index, button) in ratingButtons.enumerated() {
-      // If the index of a button is less than the rating, that button should be selected.
-      button.isSelected = index < rating
+    
+    override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        handleStarTouches(touches, withEvent: event)
     }
-  }
-  
-  // MARK: Gesture recognizer
-  
-  override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    handleStarTouches(touches, withEvent: event)
-  }
-  
-  override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    handleStarTouches(touches, withEvent: event)
-  }
-  
-  override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    delegate?.didSelectRating?(self, rating: self.rating)
-  }
-  
-  func handleStarTouches(_ touches: Set<UITouch>, withEvent event: UIEvent?) {
-    if let touch = touches.first {
-      let position = touch.location(in: self)
-      
-      if position.x > -50 && position.x < CGFloat(width + 50) {
-        ratingButtonSelected(position)
-      }
+    
+    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        delegate?.didSelectRating?(self, rating: self.rating)
     }
-  }
-  
-  func ratingButtonSelected(_ position: CGPoint) {
-    for (index, button) in ratingButtons.enumerated() {
-      if position.x > button.frame.minX {
-        self.rating = index + 1
-      } else if position.x < 0 {
-        self.rating = 0
-      }
+    
+    func handleStarTouches(_ touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.location(in: self)
+            
+            if (position.x - padding) > -50 && (position.x - padding) < CGFloat(width + 50) {
+                ratingButtonSelected(position)
+            }
+        }
     }
-  }
-  
+    
+    func ratingButtonSelected(_ position: CGPoint) {
+        for (index, button) in ratingButtons.enumerated() {
+            if (position.x + padding) > button.frame.minX + padding {
+                self.rating = index + 1
+            } else if position.x < padding {
+                self.rating = 0
+            } else {
+                
+            }
+        }
+    }
+    
 }
